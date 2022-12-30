df915236da3a19ad0ecabb7c6338ee2a69f27862
Billiere David <david.billiere@viacesi.fr>
2022-12-30 17:28:11 +0100

    Upgradeable Proxy

---
+++ contracts/Proxy-BEP20.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.6.0;
+import "@openzeppelin/contracts/proxy/TransparentUpgradeableProxy.sol";
+
+contract BEP20UpgradeableProxy is TransparentUpgradeableProxy {
+    constructor(
+        address logic,
+        address admin,
+        bytes memory data
+    ) public TransparentUpgradeableProxy(logic, admin, data) {}
+}
